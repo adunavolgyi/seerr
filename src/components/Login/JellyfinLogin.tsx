370d0ce2a7e9e26d1ddb0db059726b7f3e5dbc9a
fallenbagel <98979876+Fallenbagel@users.noreply.github.com>
2025-12-13 09:34:25 +0800

    refactor(jellyfin-login): simplify error handling for quick connect errors

--- src/components/Login/JellyfinLogin.tsx
+++ src/components/Login/JellyfinLogin.tsx
@@ -12,7 +12,7 @@ import { ApiErrorCode } from '@server/constants/error';
 import { MediaServerType, ServerType } from '@server/constants/server';
 import axios from 'axios';
 import { Field, Form, Formik } from 'formik';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { useIntl } from 'react-intl';
 import { useToasts } from 'react-toast-notifications';
 import * as Yup from 'yup';
@@ -41,10 +41,7 @@ interface JellyfinLoginProps {
   serverType?: MediaServerType;
 }
 
-const JellyfinLogin: React.FC<JellyfinLoginProps> = ({
-  revalidate,
-  serverType,
-}) => {
+const JellyfinLogin = ({ revalidate, serverType }: JellyfinLoginProps) => {
   const toasts = useToasts();
   const intl = useIntl();
   const settings = useSettings();
@@ -58,6 +55,16 @@ const JellyfinLogin: React.FC<JellyfinLoginProps> = ({
           ? ServerType.EMBY
           : 'Media Server',
   };
+
+  const handleQuickConnectError = useCallback(
+    (error: string) => {
+      toasts.addToast(error, {
+        autoDismiss: true,
+        appearance: 'error',
+      });
+    },
+    [toasts]
+  );
 
   const LoginSchema = Yup.object().shape({
     username: Yup.string().required(
@@ -233,12 +240,7 @@ const JellyfinLogin: React.FC<JellyfinLoginProps> = ({
             setShowQuickConnect(false);
             revalidate();
           }}
-          onError={(error) => {
-            toasts.addToast(error, {
-              autoDismiss: true,
-              appearance: 'error',
-            });
-          }}
+          onError={handleQuickConnectError}
           mediaServerName={mediaServerFormatValues.mediaServerName}
         />
       )}
